ef95d5a1cdf8e020d6a906c2275105d7833651dd
Radosław Eichler <agamnentzar@gmail.com>
2016-05-30 23:04:43 +0200

    Update typings, Add typings installation to postinstall script

--- typings/main.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference path="main/ambient/chai/index.d.ts" />
-/// <reference path="main/ambient/mocha/index.d.ts" />
-/// <reference path="main/ambient/node/index.d.ts" />
-/// <reference path="main/definitions/bluebird/index.d.ts" />
-/// <reference path="main/definitions/sinon/index.d.ts" />
-/// <reference path="main/definitions/ws/index.d.ts" />
