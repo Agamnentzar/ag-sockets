f2d43c3b8287ecbdb3a782629fa1a792f771b67e
Radosław Eichler <agamnentzar@gmail.com>
2016-09-30 01:20:05 +0200

    Fix issue with chai typings reference in method.d.ts

--- src/test/common.ts
+++ src/test/common.ts
@@ -1,6 +1,10 @@
 /// <reference types="mocha" />
-/// <reference types="chai" />
-/// <reference types="chai-as-promised" />
+/// <reference path="../../typings/chai.d.ts" />
+/// <reference path="../../typings/chai-as-promised.d.ts" />
 
 require('source-map-support').install();
-require('chai').use(require('chai-as-promised'));
+
+import * as chai from 'chai';
+import * as chaiAsPromised from 'chai-as-promised';
+
+chai.use(chaiAsPromised);
