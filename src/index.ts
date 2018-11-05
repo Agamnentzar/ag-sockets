f898f9afd0b23941085ed8cc3f75fb87ab72363e
Agamnentzar <agamnentzar@gmail.com>
2018-11-05 02:24:24 +0100

    Add missing export

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export * from './interfaces';
 export * from './server';
 export { createClientSocket, ClientErrorHandler } from './clientSocket';
-export { createServerRaw, createServer, broadcast } from './serverSocket';
+export { createServerRaw, createServer, broadcast, createServerHost } from './serverSocket';
 export { createClientOptions } from './serverUtils';
 export { Server, ClientState, ServerHost } from './serverInterfaces';
 export * from './method';
